d3d4c8e3d60ed36c78273ebf2ee5cd7a1caf4852
Jon Schlinkert <dev@sellside.com>
2013-07-24 13:13:26 -0400

    improve the already janky helper used for generating links

--- src/local-helpers.js
+++ src/local-helpers.js
@@ -38,13 +38,24 @@
     //   return new Handlebars.SafeString(str);
     // });
 
-    Handlebars.registerHelper('links-gh-pages', function(str) {
+    /*
+     * This helper is used to generate links for Assemble's documentation
+     * The helper simply builds a list of pages in the given directory.
+     */
+    Handlebars.registerHelper('generate-links', function(str, dir) {
+      if(isUndefined(dir)) {
+        dir = '';
+      } else {
+        dir = dir;
+      }
       var content = grunt.file.expand(str).map(function(link) {
         var page = path.basename(link).replace(/\.md\.hbs/g, '');
-        return '[' + page.toLowerCase().replace(/\s/g, '-').replace(/\./g, '-') + ']: ' + page + '.html' ;
+        return '[' + page.toLowerCase().replace(/\s/g, '-').replace(/\./g, '-') + ']: ' + dir + page + '.html' ;
       });
       return new Handlebars.SafeString(content.join('\n'));
     });
+
+
 
     Handlebars.registerHelper('links-docs', function(str) {
       var content = grunt.file.expand(str).map(function(link) {
@@ -82,6 +93,8 @@
       });
       return new Handlebars.SafeString(content.join('\n'));
     });
+
+
     Handlebars.registerHelper('links-docs-json', function(str, dir) {
       var links = {};
       var content = grunt.file.expand(str).map(function(link) {
@@ -94,8 +107,7 @@
       } else {
         dir = dir;
       }
-      result = JSON.stringify(JSON.parse(result), null, 2);
-      grunt.file.write(dir, result);
+      grunt.file.write(dir, JSON.stringify(JSON.parse(result), null, 2));
     });
 
   };
