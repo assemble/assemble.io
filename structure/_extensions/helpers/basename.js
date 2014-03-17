a8eace394ce8d7b4b297df1778d61e40998a4038
Pavel Binar <info@pavelbinar.cz>
2014-03-17 19:53:08 +0100

    Improved basename.js helper ; code done by @jonschlinkert

--- structure/_extensions/helpers/basename.js
+++ structure/_extensions/helpers/basename.js
@@ -4,10 +4,12 @@
  * Licensed under the MIT License (MIT).
  */
 
+var path = require('path');
+
 module.exports.register = function (Handlebars) {
   'use strict';
 
   Handlebars.registerHelper("basename", function(filepath) {
-    return require('fs-utils').name(filepath);
+    return path.basename(filepath);
   });
 };
