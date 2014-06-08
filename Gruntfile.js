81ad2c03824f6a9235a2c8dc15dbc995198884e6
Patrick Burtchaell <patrick@pburtchaell.com>
2014-06-08 11:02:11 -0500

    Update gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,33 @@
-
 /*
  * assemble.io <https://github.com/assemble/assemble.io>
  *
  * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
  * Licensed under the MIT license.
  */
-'use strict';
 
+'use strict';
 
 var resolve = require('resolve-dep');
 var argv = require('minimist')(process.argv.slice(2));
 var modeFlag = argv.m || argv.mode;
 var deployFlag = argv.deploy;
 
-// This also uses grunt's debug
-// var debug = argv.debug;
-
-
 module.exports = function(grunt) {
 
-  // Force unix-style newlines
-  grunt.util.linefeed = '\n';
+  
+  grunt.util.linefeed = '\n'; // Force unix-style newlines
+  grunt.util._.mixin(require('./data/_utils/utils')); // Mix utils into Lo-Dash
 
-  // Mix utils into Lo-Dash
-  grunt.util._.mixin(require('./data/_utils/utils'));
-
-  // Report the execution time of each task.
-  require('time-grunt')(grunt);
+  require('time-grunt')(grunt); // Report the execution time of each task.
 
   // Initialize Grunt configuration
   grunt.initConfig({
 
-    // Project Metadata
+    /** 
+     * Project Metadata:
+     *
+     * .assemblerc.yml: all Assemble.js confifguration
+     */
     site     : grunt.file.readYAML('.assemblerc.yml'),
     pkg      : grunt.file.readJSON('package.json'),
     vendor   : grunt.file.readJSON('.bowerrc').directory,
@@ -60,38 +56,43 @@ module.exports = function(grunt) {
     },
 
     /**
-     * HTML tasks
+     * 1.0 HTML compilation tasks:
+     * 
+     * 1.1 _Assemble_ task will take Markdown, YAML, and JSON content
+     * and compile HTML files.
+     * 
+     * 1.2 _Validation_ task will validate compiled HTML. 
+     * 
+     * 1.3 _Prettify_ task will take compiled HTML and remove extra lines
+     * and spaces, fix indention, etc.
      */
-
-    // Build HTML from templates and data
     assemble: {
       options: {
         flatten: true,
         assets: '<%= site.public %>',
         css: '<%= less.docs.dest %>',
-
-        // Metadata
         pkg: '<%= pkg %>',
         site: '<%= site %>',
-        _assemble: '<%= _assemble %>', // from github.com/assemble/assemble
+        _assemble: '<%= _assemble %>',
+        
         data: [
-          {name: ':basename', src: ['<%= site.data %>/{,*/}*.{json,yml}']}
+          { 
+            name: ':basename', 
+            src: ['<%= site.data %>/{,*/}*.{json,yml}']
+          }
         ],
-
-        // Template includes
+          
         partials: [
           '<%= site.components %>/{,*/}*.hbs',
           '<%= site.includes %>/{,*/}*.hbs',
           '<%= site.snippets %>/*.hbs',
           '<%= site.content %>/**/*.md'
         ],
-
-        // Templte layouts
+          
         layoutdir: '<%= site.layouts %>',
         layoutext: '<%= site.layoutext %>',
         layout: '<%= site.layout %>',
 
-        // Assemble extensions
         mixins: ['<%= site.mixins %>/utils.js'],
         helpers: ['<%= load.helpers %>', '<%= site.helpers %>/*.js'],
         middleware: ['<%= load.middleware %>'],
@@ -116,12 +117,12 @@ module.exports = function(grunt) {
       },
       site: {
         options: {theme: 'docs'},
-        files: {'<%= site.dest %>/': ['<%= site.pages %>/helpers.hbs']}
+        files: {
+          '<%= site.dest %>/': ['<%= site.pages %>/helpers.hbs']
+        }
       }
     },
 
-
-    // Lint HTML
     validation: {
       options: {
         reset: true,
@@ -139,24 +140,37 @@ module.exports = function(grunt) {
       }
     },
 
-    // Prettify HTML
     prettify: {
       options: {
         indent_scripts: 'keep'
       },
       site: {
         files: [
-          {expand: true, cwd: '<%= site.dest %>', src: '{,*/}*.html', dest: '<%= site.dest %>/', ext: '.html'}
+          {
+            expand: true, 
+            cwd: '<%= site.dest %>', 
+            src: '{,*/}*.html', 
+            dest: '<%= site.dest %>/', 
+            ext: '.html'
+          }
         ]
       }
     },
-
-
+    
     /**
-     * CSS tasks
+     * 2.0 Stylesheet tasks:
+     * 
+     * 2.1 _less_ task will compile CSS stylesheets
+     * from LESS source code.
+     *
+     * 2.2 _autoprefixer_ task will make sure compiled
+     * stylesheet supports the following browsers:
+     *   - Last 2 versions of all major browsers
+     *   - Internet Explorer 9
+     *   - Internet Explorer 8
+     *
+     * 2.3 _csslint_
      */
-
-    // Compile Less to CSS
     less: {
       options: {
         paths: [
@@ -167,18 +181,15 @@ module.exports = function(grunt) {
       },
       docs: {
         options: {
-          globalVars: {theme: 'docs'}
+          globalVars: { 
+            theme: 'docs'
+          }
         },
         src: ['<%= site.styles %>/index.less'],
         dest: '<%= site.public %>/css/docs.css'
       },
-      // blog: {
-      //   options: {
-      //     globalVars: {theme: 'blog'}
-      //   },
-      //   src: ['<%= less.docs.src %>'],
-      //   dest: '<%= site.public %>/css/blog.css'
-      // }
+    },
+    
     autoprefixer: {
       options: {
         browsers: ['last 2 version', 'ie 8', 'ie 9']
@@ -191,7 +202,6 @@ module.exports = function(grunt) {
       },
     },
 
-    // Lint CSS
     csslint: {
       strict: {
         options: {
@@ -201,7 +211,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // Prettify CSS
     csscomb: {
       options: {
         config: 'styles/.csscomb.json'
@@ -211,8 +220,7 @@ module.exports = function(grunt) {
         dest: '<%= less.docs.dest %>'
       }
     },
-
-    // Remove unused CSS from output.
+    
     uncss: {
       options: {
         ignore: ['#webfonts', '.active']
@@ -225,35 +233,43 @@ module.exports = function(grunt) {
 
 
     /**
-     * JavaScript tasks
+     * 3.0 JavaScript tasks:
+     * 
+     * 3.1 _jslint_ task will lint all scripts against
+     * the rules in ".jshintrc".
+     *
+     * 3.2 _uglify_ task will concatentate scripts
+     *    3.2.1 docs.js - primary scripts for /docs
+          3.2.1 ie.js - provides IE8 support
      */
-
-    // Lint JavaScripts
     jshint: {
       options: {
         jshintrc: '<%= site.scripts %>/.jshintrc'
       },
       all: [
         'Gruntfile.js',
-        '<%= site.helpers.path %>/*.js',
+        '<%= site.helpers.path %>/*.js'
         // '<%= site.plugins.path %>/*.js',
         // '<%= site.scripts %>/{,*/}*.js',
         // '<%= site.utils %>/{,*/}*.js',
       ]
     },
 
-    // Minify JavaScripts
     uglify: {
       options: {
         banner: '<%= metadata.banner %>'
       },
-      site: {
+      
+      /** 
+       * 3.2.1 "docs.js" 
+       */
+      docs: {
         src: ['<%= site.scripts %>/{,*/}*.js'],
         dest: '<%= site.public %>/js/docs.min.js'
       },
-
+      
       /**
-       * ie.js
+       * 3.2.2 "ie.js"
        * Provides support in older versions of IE.
        * Source scripts are installed via Bower.
        */
@@ -268,33 +284,44 @@ module.exports = function(grunt) {
 
 
     /**
-     * "setup" tasks
+     * 4.0 intial project tasks
+     *
+     * 4.1 _copy_ task
+     *    4.1.1 vendor - Copy Bootstrap's js to local scripts
+     *    4.1.2 assets - Copy local fonts and images to `public` directory
+     * 
+     * 4.2 _clean_ task will clean files from previous build.
      */
-
     copy: {
-      // Copy Bootstrap's js to local scripts
       vendor: {
         src: '<%= bootstrap.js %>/bootstrap.js',
         dest: '<%= site.scripts %>/vendor/bootstrap.js'
       },
-      // Copy local `assets` to `public` directory
       assets: {
         files: [
-          {expand: true, cwd: '<%= site.assets %>', src: '**', dest: '<%= site.public %>/'}
+          {
+            expand: true, 
+            cwd: '<%= site.assets %>', 
+            src: '**', 
+            dest: '<%= site.public %>/'
+          }
         ]
       }
     },
-
-    // Clean files from previous build
+    
     clean: {
       example: ['<%= site.dest %>/{,*/}*.{html,css,js}']
     },
 
     /**
-     * "Live" tasks
+     * 5.0 Assemble Developer tasks
+     *
+     * 5.1 _connect_ task runs a local server at port 9000
+     *
+     * 5.2 _watch_ task watched source files for changes
+     *    5.2.1 content - watch Markdown, Handlebars and source data files and run `assemble` task on change.
+     *    5.2.2 styles - watch LESS source code and run `less` task on change.
      */
-
-    // Run a Connect server
     connect: {
       options: {
         port: 9000,
@@ -308,11 +335,12 @@ module.exports = function(grunt) {
         }
       }
     },
-
-    // Watch certain files, rebuild when changes are saved.
+    
     watch: {
-      options: {livereload: true},
-      html: {
+      options: {
+        livereload: true
+      },
+      content: {
         tasks: ['assemble'],
         files: [
           '<%= site.templates %>/{,*/}*.hbs',
@@ -320,7 +348,7 @@ module.exports = function(grunt) {
           '<%= site.data %>/{,*/}*.{yml,json}'
         ]
       },
-      css: {
+      styles: {
         tasks: ['less'],
         files: [
           '<%= site.styles %>/{,*/}*.less'
@@ -340,70 +368,43 @@ module.exports = function(grunt) {
     }
   });
 
-  // check for a command line option to deploy
-  if (deployFlag) {
-    grunt.config.set('site.root', '_deploy');
-  }
-
-
-  if (modeFlag) {
-    grunt.config.set('site.mode', modeFlag);
-  }
-
-
-  /**
-   * Load Plugins
+  /** 
+   * Check for a command line option to deploy
+   *
+   * deployFlag -
+   * modeFlag - 
    */
+  if (deployFlag) { 
+    grunt.config.set('site.root', '_deploy'); 
+  }
+  if (modeFlag) { 
+    grunt.config.set('site.mode', modeFlag); 
+  }
 
-  // Load npm plugins to provide necessary tasks.
+  /* Load all modules listed in package.json Plugins with load-grunt-tasks */
   require('load-grunt-tasks')(grunt);
 
   /**
-   * Tasks to be run
+   * 6.0 Primary tasks
+   * 
    */
-
-  // First things first.
-  grunt.registerTask('prep', [
-    'clean',
-    'copy',
-  ]);
-
-  // Compile CSS, HTML, etc.
-  grunt.registerTask('compile', [
-    'less',
-    'assemble'
-  ]);
-
-  // Lint and runt tests.
-  grunt.registerTask('lint', [
-    'jshint',
-    // 'validation',
-    // 'csslint'
-  ]);
-
-  // Prep for deployment.
-  grunt.registerTask('prod', [
-    'uglify'
-  ]);
-
-  // Lint and runt tests.
-  grunt.registerTask('document', [
-    'verb'
-  ]);
-
+  grunt.registerTask('prep', ['clean','copy']); // Clean dest folder
+  grunt.registerTask('compile', ['less','assemble']); // Compile CSS, HTML, etc.
+  grunt.registerTask('lint', ['jshint',]); // Lint and runt tests.
+  grunt.registerTask('prod', ['uglify']);
+  grunt.registerTask('document', ['verb']); // Compile README.md
 
   /**
-   * Tasks to be run
+   * 7.0 Secondary tasks
+   *
+   * Use `grunt design` for development of the site.
+   * Use `grunt` to compile a production ready version of the site.
    */
-
-  // Design-oriented tasks.
   grunt.registerTask('design', [
     'default',
     'connect',
     'watch:design'
   ]);
-
-  // Default tasks to run with the `grunt` command.
   grunt.registerTask('default', [
     'prep',
     'lint',
